8b9d639093d62983db75297741c9a23c794e59be
Ivan-Clare <ngongivan@gmail.com>
2019-09-30 23:49:52 +0200

    test to buildspec.yml

--- test_main.py
+++ test_main.py
@@ -24,9 +24,9 @@ def client():
 
 def test_health(client):
     response = client.get('/')
-    assert response.status_code == 200
-    assert response.json == 'Healthy'
-    assert False
+    assert response.status_code == 404
+    assert response.json == 'UnHealthy'
+   
 
 def test_auth(client):
     body = {'email': EMAIL,
